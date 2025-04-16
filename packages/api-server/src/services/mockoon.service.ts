8664fd0c510a0c0a7727d39b371b4731a866b4bf
GreenRenge <greenrenge@gmail.com>
2025-04-16 11:28:30 +0700

    feat: mock user enterprise plan

--- packages/api-server/src/services/mockoon.service.ts
+++ packages/api-server/src/services/mockoon.service.ts
@@ -1,5 +1,37 @@
 import { mustLogin } from '../mixins/mustLogin'
 import { AuthContextMeta } from '../types/common'
+// export type UserProfile = {
+// 	displayName?: string;
+//       };
+
+//       export type User = {
+// 	uid: string;
+// 	email: string;
+// 	plan: Plans;
+// 	teamId: string;
+// 	teamRole: TeamRoles;
+// 	deployInstancesQuota: number;
+// 	deployInstancesQuotaUsed: number;
+// 	cloudSyncItemsQuota: number;
+// 	cloudSyncItemsQuotaUsed: number;
+// 	cloudSyncSizeQuota: number;
+// 	cloudSyncHighestMajorVersion: number | null;
+// 	templatesQuota: number;
+// 	templatesQuotaUsed: number;
+// 	nextQuotaResetOn: number;
+// 	subscription: {
+// 	  trial?: boolean;
+// 	  provider?: 'stripe' | 'paddle' | 'free' | 'manual';
+// 	  frequency?: Frequency;
+// 	  createdOn: number;
+// 	  renewOn: number;
+// 	  portalEnabled?: boolean;
+// 	  cancellationScheduled?: boolean;
+// 	  pastDue?: boolean;
+// 	  subscriptionId: string;
+// 	};
+//       } & UserProfile;
+
 export default {
 	name: 'mockoon',
 	version: 1,
@@ -12,10 +44,35 @@ export default {
 			rest: 'GET /user',
 			visibility: 'published',
 			handler(ctx: AuthContextMeta) {
-				const user = ctx.meta.user
-				console.log('ctx', ctx.meta)
+				const user = ctx.meta.user!
+
 				return {
-					...user,
+					uid: user.id,
+					email: user.email,
+					plan: 'ENTERPRISE',
+					teamId: 'F1',
+					teamRole: 'owner',
+					deployInstancesQuota: 3,
+					deployInstancesQuotaUsed: 0,
+					cloudSyncItemsQuota: 999,
+					cloudSyncItemsQuotaUsed: 0,
+					cloudSyncSizeQuota: 15000000, // 15MB
+					cloudSyncHighestMajorVersion: 1,
+					templatesQuota: 999,
+					templatesQuotaUsed: 0,
+					nextQuotaResetOn: 1,
+					subscription: {
+						trial: false,
+						provider: 'manual',
+						frequency: 'YEARLY',
+						createdOn: 1744777380227,
+						renewOn: 4079996600352,
+						portalEnabled: true,
+						cancellationScheduled: false,
+						pastDue: false,
+						subscriptionId: 'F1',
+					},
+					displayName: user.user_metadata?.preferred_username || user.user_metadata?.full_name,
 				}
 			},
 		},
@@ -25,9 +82,9 @@ export default {
 			handler() {
 				return {
 					enableTelemetry: false,
-					geoipEndpoint: '',
-					cloudSyncUrl: '',
-					deployUrl: '',
+					geoipEndpoint: 'http://ip-api.com/json/',
+					cloudSyncUrl: 'ws://localhost:5003',
+					deployUrl: 'http://localhost:6000', // TODO: GREEN
 				}
 			},
 		},
